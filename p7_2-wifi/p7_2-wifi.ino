db931166a67d7f1d15b51b5ca9c026d8e2c02273
Alfredo Renteria <alfredorenteria@MacBook-Air-de-Alfredo.local>
2022-08-08 20:23:36 -0500

    Add p7.2

---
+++ p7_2-wifi/p7_2-wifi.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+String my_ssid = "ESP8266-IoT";
+String my_password = "asdf1234";
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(my_ssid, my_password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+
+}
